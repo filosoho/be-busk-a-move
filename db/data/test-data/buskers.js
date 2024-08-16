fea0b07995f8e6d4833612c06ac42bcd54865549
Anna <filosoho@gmail.com>
2024-08-16 14:35:00 +0100

    add buskers data to development and test data folders

---
+++ db/data/test-data/buskers.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    busk_location: {
+      latitude: 31.1369,
+      longitude: 147.7351,
+    },
+    busk_location_name: "National City",
+    busk_time: 1723795428420,
+    username: "Donato77",
+    user_image_url: "https://avatars.githubusercontent.com/u/87704933",
+    busk_about_me:
+      "Torqueo adnuo thalassinus sonitus volubilis aggredior vito canis non. Amitto cicuta cruciamentum auctor testimonium. Ex antiquus defungo varietas caelestis voro pariatur clamo.",
+    busk_setup: "Ante adimpleo vel commemoro demo ab appono volva.",
+  },
+  {
+    busk_location: {
+      latitude: -4.2768,
+      longitude: 168.4704,
+    },
+    busk_location_name: "Southaven",
+    busk_time: 1723750242682,
+    username: "Bella35",
+    user_image_url: "https://avatars.githubusercontent.com/u/50587032",
+    busk_about_me:
+      "Aperte absum universe illo placeat pecto tolero. Statua tardus defleo victus bellum adipisci commodi officia. Ancilla terreo consequuntur comedo coerceo fuga socius nihil tubineus.",
+    busk_setup: "Tametsi velum thermae carus vulpes voluntarius maxime civis.",
+  },
+  {
+    busk_location: {
+      latitude: 11.1017,
+      longitude: 124.3153,
+    },
+    busk_location_name: "Pietrocester",
+    busk_time: 1723772450448,
+    username: "Loma_Luettgen",
+    user_image_url:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/776.jpg",
+    busk_about_me:
+      "Sit caterva consuasor contra degero sordeo totam vomer theca acidus. Denego teres tempore abutor tumultus veniam voluptatem impedit totam. Confugo comis voluptatum addo solutio bene crur.",
+    busk_setup: "Curia vir cimentarius spero expedita quisquam vacuus apto.",
+  },
+  {
+    busk_location: {
+      latitude: 15.1383,
+      longitude: -166.1735,
+    },
+    busk_location_name: "Romaguerastead",
+    busk_time: 1723750567234,
+    username: "Jackson.Mills",
+    user_image_url:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/765.jpg",
+    busk_about_me:
+      "Non admitto aliqua desolo totam depono tonsor. Culpo synagoga territo compono adulatio inflammatio soluta. Desparatus caute casus.",
+    busk_setup: "Sint adopto aperiam aliqua alius stips vesica cum.",
+  },
+];
