65b8b7b304625a4ad194f8939208cb799a6ab99d
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-08-19 11:04:27 +0100

    get busks logic implemented, and testing has been set up

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const endpoints = require("./endpoints.json");
+const apiRouter = require('./routers/api-router')
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/api", (req, res, next) => {
-  res.status(200).send({ endpoints: endpoints });
-});
+app.use('/api', apiRouter)
 
 module.exports = app;
