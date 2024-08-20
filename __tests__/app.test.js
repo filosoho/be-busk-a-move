2f685cde800d503fa34de01aaf40361a0e866eaf
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-08-20 11:27:37 +0100

    all post and get busk logic fully merged

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,16 +12,72 @@ afterAll(() => {
 	return db.end();
 });
 
-describe("GET /api/busks", () => {
-	it("GET 200: should respond with a 200 status code and return an array of busks to the client", () => {
-		return request(app)
-			.get("/api/busks")
-			.expect(200)
-			.then((response) => {
-				expect(response.body.busks.length).toBeGreaterThan(0);
-			});
-	});
-});
+describe('GET /api/busks' ,() => {
+    it('GET 200: should respond with a 200 status code and return an array of busks to the client', () => {
+        return request(app)
+        .get('/api/busks')
+        .expect(200)
+        .then((response) => {
+            const busks = response.body.busks
+            expect(busks.length).toBeGreaterThan(0)
+            expect(Array.isArray(busks)).toBe(true)
+            busks.forEach((busk) => {
+                expect(busk).toHaveProperty('busk_id')
+                expect(busk.busk_id).toBeNumber()
+                expect(busk).toHaveProperty('busk_location')
+                expect(busk.busk_location).toBeObject()
+                expect(busk).toHaveProperty('busk_location_name')
+                expect(busk.busk_location_name).toBeString()
+                expect(busk).toHaveProperty('busk_time')
+                expect(busk.busk_time).toBeString()
+                expect(busk).toHaveProperty('busk_date')
+                expect(busk.busk_date).toBeString()
+                expect(busk).toHaveProperty('username')
+                expect(busk.username).toBeString()
+                expect(busk).toHaveProperty('user_image_url')
+                expect(busk.user_image_url).toBeString()
+                expect(busk).toHaveProperty('busk_about_me')
+                expect(busk.busk_about_me).toBeString()
+                expect(busk).toHaveProperty('busk_setup')
+                expect(busk.busk_setup).toBeString()
+            })
+        })
+    })
+})
+
+describe('POST /api/busks', () => {
+    it('should respond with a 201 status code and add a busk to the database', () => {
+      const newBusk = {
+        busk_location: { latitude: 40.7128, longitude: -74.0060 },
+        busk_location_name: 'Central Park',
+        busk_time: '15:00:00', 
+        busk_date: null,
+        username: 'Jodie_Bednar22',
+        user_image_url: 'http://example.com/image.jpg',
+        busk_about_me: 'Looking for musicians!',
+        busk_setup: 'Guitar and vocals',
+      };
+  
+      return request(app)
+        .post('/api/busks')
+        .send(newBusk)
+        .expect(201)
+        .then(response => {
+          expect(response.body.busk).toEqual({
+            busk_location: { latitude: 40.7128, longitude: -74.0060 },
+            busk_location_name: 'Central Park',
+            busk_time: '15:00:00', 
+            busk_date: null,
+            busk_id: 5,
+            username: 'Jodie_Bednar22',
+            user_image_url: 'http://example.com/image.jpg',
+            busk_about_me: 'Looking for musicians!',
+            busk_setup: 'Guitar and vocals',
+          })
+  		 })
+       });
+   });
+
 
 describe("GET /api/busks/:busk_id", () => {
 	test("GET 200: should return the specified busk object", () => {
--- controllers/busks.controllers.js
+++ controllers/busks.controllers.js
@@ -1,3 +1,5 @@
+const {selectBusks, addBusk} = require('../models/busks.models')
+
 const {
   selectBusks,
   addBusk,
@@ -21,60 +23,14 @@ exports.getBusksById = (req, res, next) => {
     });
 };
 
+
 exports.postBusk = (req, res, next) => {
-<<<<<<< HEAD
-  const {
-    busk_location,
-    busk_location_name,
-    busk_time,
-    username,
-    user_image_url,
-    busk_about_me,
-    busk_setup,
-  } = req.body;
 
-  if (
-    !busk_location ||
-    !busk_location_name ||
-    !busk_time ||
-    !username ||
-    !user_image_url ||
-    !busk_about_me ||
-    !busk_setup
-  )
-    return Promise.reject({
-      status: 404,
-      message: "Bad request: missing required fields",
-    });
-
-  addBusk({
-    busk_location,
-    busk_location_name,
-    busk_time,
-    username,
-    user_image_url,
-    busk_about_me,
-    busk_setup,
-  }).then((busk) => {
-    res
-      .status(201)
-      .send({ busk })
-      .catch((err) => {
-        next(err);
-      });
-  });
+  addBusk(req.body)
+  .then(busk => {
+      res.status(201).send({busk})
+  })
+  .catch(err => {
+      next(err)
+  })
 };
-=======
-    const {busk_location, busk_location_name, busk_time, username, user_image_url, busk_about_me, busk_setup} = req.body
-
-    if(!busk_location || !busk_location_name || !busk_time|| !username || !user_image_url || !busk_about_me || !busk_setup)
-        return Promise.reject({status: 404, message:'Bad request: missing required fields'})
-
-    addBusk({busk_location, busk_location_name, busk_time, username, user_image_url, busk_about_me, busk_setup})
-    .then((busk) => {
-        res.status(201).send({busk}).catch((err) => {
-            next(err)
-        })
-    })
-};
->>>>>>> 79601c3d91da1364bcc1346cd2b6528665ae6d4d
