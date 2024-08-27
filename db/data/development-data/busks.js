b8ac5bd0f7baba23ee399b2dcd760298147da182
BassManLuke <luke_haz@yahoo.co.uk>
2024-08-27 15:35:13 +0100

    update busk locations with Leeds locations

--- db/data/development-data/busks.js
+++ db/data/development-data/busks.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     busk_location: {
-      latitude: 79.0359,
-      longitude: 59.2569,
+      latitude: 53.7964808712088,
+      longitude: -1.5477046885297452,
     },
-    busk_location_name: "Legrostown",
+    busk_location_name: "Leeds City Sqaure",
     busk_time_date: "2024-08-14",
     username: "Barry57",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1053.jpg",
+      "https://images.unsplash.com/photo-1484972759836-b93f9ef2b293?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Video solium ad utrimque sodalitas. Comitatus demulceo ascisco sol amo clementia tero argumentum ventus repellendus. Ullam beneficium somniculosus.",
     busk_setup: "Consequuntur thorax vigilo.",
@@ -16,13 +16,13 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: 7.6494,
-      longitude: -174.1609,
+      latitude: 53.79884749002853,
+      longitude: -1.5516932209308096,
     },
-    busk_location_name: "Coltton",
+    busk_location_name: "Leeds Park Square",
     busk_time_date: "2024-08-15",
     username: "Stanford41",
-    user_image_url: "https://avatars.githubusercontent.com/u/22509411",
+    user_image_url: "https://images.unsplash.com/photo-1591143903248-883d5f2a9fdb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Comburo molestiae decet beatus depulso supra sequi vado. Congregatio ager fugiat spero. Amissio aliquid maiores venustas vitium deleniti cupiditate libero decet.",
     busk_setup:
@@ -31,13 +31,13 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: -33.2551,
-      longitude: 29.4709,
+      latitude: 53.796926081114016,
+      longitude: -1.542362167167193,
     },
-    busk_location_name: "New Wardstad",
+    busk_location_name: "Briggate - Outside M&S",
     busk_time_date: "2024-08-16",
     username: "Unique_Friesen",
-    user_image_url: "https://avatars.githubusercontent.com/u/41031320",
+    user_image_url: "https://images.unsplash.com/photo-1519987505431-a451e51c437b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Despecto maiores tego voluptatibus saepe textilis versus a. Vergo vorax virgo vorago aro cerno defluo curis recusandae caterva. Sed adipisci defero debeo talis caries temperantia.",
     busk_setup: "Truculenter stillicidium reprehenderit derideo.",
@@ -45,14 +45,14 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: -26.5962,
-      longitude: 103.0098,
+      latitude: 53.79822089885126,
+      longitude: -1.5392019727524078,
     },
-    busk_location_name: "Jacobsfort",
+    busk_location_name: "Outside Victoria Shopping Centre",
     busk_time_date: "2024-08-17",
     username: "Wilfred_Koch",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/12.jpg",
+      "https://images.unsplash.com/photo-1534076022384-b0755a10293b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Bene thesaurus uterque auditor tantum. Approbo veniam tres vero dicta. Alveus tertius curvo tergeo appono usque apostolus adfectus possimus defessus.",
     busk_setup: "Pauci custodia cinis.",
@@ -67,7 +67,7 @@ module.exports = [
     busk_time_date: "2024-08-18",
     username: "Faye54",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/166.jpg",
+      "https://images.unsplash.com/photo-1590581296894-3c897baa0e54?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Decerno aegre vulnus vorago coerceo censura veniam. Adfectus cohaero sustineo trado animus. Alo deorsum ullam.",
     busk_setup: "Centum auditor illo utroque adfero terra vallum.",
@@ -75,14 +75,14 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: 64.031,
-      longitude: 82.3083,
+      latitude: 53.79735333420201,
+      longitude: -1.5452442418047234,
     },
-    busk_location_name: "Nashville-Davidson",
+    busk_location_name: "Outside Pizza Punks",
     busk_time_date: "2024-08-19",
     username: "Lincoln_Bogisich",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1002.jpg",
+      "https://images.unsplash.com/photo-1486808373800-adfc9a3fe20b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Carcer adduco apparatus. Quae turba condico celer cognatus ceno. Debilito charisma vitiosus.",
     busk_setup: "Vitae arcus brevis vox uter abscido complectus.",
@@ -95,13 +95,14 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: 76.0721,
-      longitude: 168.1681,
+      latitude: 53.79347494872672,
+      longitude: -1.5490587361195314,
+
     },
-    busk_location_name: "Bereniceboro",
+    busk_location_name: "Granary Wharf",
     busk_time_date: "2024-08-20",
     username: "Wilson.Doyle",
-    user_image_url: "https://avatars.githubusercontent.com/u/12148888",
+    user_image_url: "https://images.unsplash.com/photo-1579090423747-52a4ff1a6a21?q=80&w=1108&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Illo vix vulgo patria. Pecus antepono tui approbo demitto surgo abundans ullus. Arceo rerum cognatus curso comedo aut labore caput aspicio nam.",
     busk_setup:
@@ -116,14 +117,14 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: 43.989,
-      longitude: -146.9876,
+      latitude: 53.799708441891646,
+      longitude: -1.5438896267396192,
     },
-    busk_location_name: "Bergebury",
+    busk_location_name: "Dortmund Square",
     busk_time_date: "2024-08-21",
     username: "Santa_Rowe",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1148.jpg",
+      "https://images.unsplash.com/photo-1525994886773-080587e161c2?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Audio sopor spiculum iste deripio considero stips. Quisquam vulnero ad cado considero. Allatus abeo defero civis cruciamentum summisse.",
     busk_setup: "Correptius accedo eos canonicus illo paens.",
@@ -131,13 +132,13 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: -79.455,
-      longitude: 109.8919,
+      latitude: 53.79605802716369,
+      longitude: -1.5405300188700874,
     },
-    busk_location_name: "Lake Christ",
+    busk_location_name: "Outside Corn Exchange",
     busk_time_date: "2024-08-22",
     username: "Sasha_Quitzon",
-    user_image_url: "https://avatars.githubusercontent.com/u/33110706",
+    user_image_url: "https://images.unsplash.com/photo-1489582636924-5b46e9fa372c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Explicabo maxime sopor. Velum annus incidunt baiulus stipes calco auctus desidero comprehendo. Audacia aureus dedico vulariter ait vinum surgo alveus articulus.",
     busk_setup: "Curso commodi umbra appono aliqua voluptate callide quam.",
@@ -145,14 +146,14 @@ module.exports = [
   },
   {
     busk_location: {
-      latitude: -9.1104,
-      longitude: 103.6688,
+      latitude: 53.79655723831467,
+      longitude: -1.5438368291954374,
     },
     busk_location_name: "Port Natasha",
     busk_time_date: "2024-08-23",
     username: "Sincere2",
     user_image_url:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/536.jpg",
+      "https://images.unsplash.com/photo-1532959801411-cf28447984f9?q=80&w=990&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     busk_about_me:
       "Ubi turpis terror defessus adicio. Credo defero venio decimus vix apud. Claro tergeo consequatur ambitus crepusculum.",
     busk_setup: "Aspicio demens uredo.",
